800b24b101428ca60d4f1e0781cafb99d22845bb
RandomPGR301 <random@mail.com>
2018-12-02 22:37:31 +0100

    small app, testing logz.io

--- src/main/java/com/example/herokupipeexample/CustomerController.java
+++ src/main/java/com/example/herokupipeexample/CustomerController.java
@@ -2,6 +2,9 @@ package com.example.herokupipeexample;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     private CustomerRepository customerRepository;
+    private Logger logger = LoggerFactory.getLogger(CustomerController.class);
+    private int counter;
 
     public CustomerController(CustomerRepository customerRepository) {
       this.customerRepository = customerRepository;
@@ -19,6 +24,22 @@ public class CustomerController {
 
     @RequestMapping("/")
     public String welcome() {
+
+        logger.info("Testing logz.io!");
+        logger.warn("Winter is coming");
+
+        logger.error("Testing error #" + counter);
+        logger.warn("Testing warn #" + counter);
+        logger.info("Testing info #" + counter);
+        logger.debug("Testing debug #" + counter);
+
+        System.out.println("Testing error #" + counter);
+        System.out.println("Testing warn #" + counter);
+        System.out.println("Testing info #" + counter);
+        System.out.println("Testing debug #" + counter);
+
+        counter++;
+
         return "Welcome to this small REST service. It will accept a GET on /list with a request parameter lastName, and a POST to / with a JSON payload with firstName and lastName as values.";
     }
 
